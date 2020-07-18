2d1b991bfea3c1010d077f1bf884d34815f616cb
Abhilash CR <abhilash.cr@mountblue.tech>
2020-07-18 15:00:08 +0530

    correction after first review

--- src/server/index.js
+++ src/server/index.js
@@ -1,22 +1,29 @@
+//importing dependency
 const csvtojson = require("csvtojson");
 const ctj= require("convert-csv-to-json");
+const func = require('./ipl.js');
 const fs= require("fs");
+//converting csv data to Json object
 let deliveries = ctj.fieldDelimiter(',').formatValueByType().getJsonFromCsv("..//data//deliveries.csv");
 let matches = ctj.fieldDelimiter(',').formatValueByType().getJsonFromCsv("..//data//matches.csv");
+//finding starting match id of the year 2015 and 2016
 id2016= matches.findIndex( a => a.season===2016)
 id2015= matches.findIndex( a=> a.season===2015)
-//console.log(id2016,id2015)
-const func = require('./ipl.js');
-let m=func.noOfMatchWonPerYear(matches)
-let w=func.noOfMatchesPerYear(matches)
-//console.log(w[2016],w[2015])
-let r=func.extraruns(deliveries,id2016,id2016+w[2016])
-let out4=func.bowlereco(deliveries,id2015,id2015+w[2015])
-fs.writeFile('..//output//matchesPerYear.json',JSON.stringify(w), (err)=>
+
+//calling function
+let _matchWonPerYear=func.noOfMatchWonPerYear(matches)
+let _matchPerYear=func.noOfMatchesPerYear(matches)
+let _extraRuns2016=func.extraRuns2016(deliveries,id2016,id2016+_matchPerYear[2016])
+let _topTenEcoBowler2015=func.topTenEcoBowler2015(deliveries,id2015,id2015+_matchPerYear[2015])
+
+
+//writeing in to output file
+fs.writeFile('..//output//matchesPerYear.json',JSON.stringify(_matchPerYear), (err)=>
 console.log("matchesPerYear File written"));
-fs.writeFile('..//output//matchesWonPerTeamPerYear.json',JSON.stringify(m), (err)=>
+fs.writeFile('..//output//matchesWonPerTeamPerYear.json',JSON.stringify(_matchWonPerYear), (err)=>
 console.log("matchesWonPerTeamPerYear File written"));
-fs.writeFile('..//output//ExtraRunsPerTeam2016.json',JSON.stringify(r), (err)=>
+fs.writeFile('..//output//ExtraRunsPerTeam2016.json',JSON.stringify(_extraRuns2016), (err)=>
 console.log("ExtraRunsPerTeam2016 File written"));
-fs.writeFile('..//output//Top10EcoBowlers.json',JSON.stringify(out4), (err)=>
+fs.writeFile('..//output//Top10EcoBowlers.json',JSON.stringify(_topTenEcoBowler2015), (err)=>
 console.log("Top10EcoBowlers File written"));
+
--- src/server/ipl.js
+++ src/server/ipl.js
@@ -1,84 +1,98 @@
 function noOfMatchesPerYear(matches){
-    let m={}
+    let noOfMatches={}
     matches.forEach( a => {
-        if(m.hasOwnProperty(a.season)){
-            m[a.season]+=1;
+        if(noOfMatches.hasOwnProperty(a.season)){
+            noOfMatches[a.season]+=1;
         }
         else{
-            m[a.season]=1;
+            noOfMatches[a.season]=1;
         }
-
-      
     });
-    return m
-    //console.log(m)
+    return noOfMatches
 }
+
+
+
 function noOfMatchWonPerYear(matches){
-    w={}
+    let noOfMatchesWon={}
     matches.forEach( a => {
-    if(a.result==='normal'){
-        if(!w.hasOwnProperty(a.season)){
-            w[a.season]={}
-            w[a.season][a.winner]=0;
-        }
-        if(w.hasOwnProperty(a.season) && w[a.season].hasOwnProperty(a.winner)){
-            w[a.season][a.winner]+=1;
-        }
-        else{
-            w[a.season][a.winner]=1;
-        }
-    }
-    })
-    return w;
+        if(a.result==='normal'){
+            if(!noOfMatchesWon.hasOwnProperty(a.season)){
+                noOfMatchesWon[a.season]={}
+                noOfMatchesWon[a.season][a.winner]=0;
+            }
+            if(noOfMatchesWon.hasOwnProperty(a.season) && noOfMatchesWon[a.season].hasOwnProperty(a.winner)){
+                noOfMatchesWon[a.season][a.winner]+=1;
+            }
+            else{
+                noOfMatchesWon[a.season][a.winner]=1;
+            }
+        }   
+    });
+    return noOfMatchesWon;
 }
-function extraruns(deliveries,id2016,end){
-    r={}
+
+
+function extraRuns2016(deliveries,id2016,end){
+    let extraruns={}
     deliveries.forEach( a =>{
-      if(a.match_id >=id2016+1 && a.match_id<=(id2016+end)){
-        if(r[a.bowling_team]){
-            r[a.bowling_team]+=a.extra_runs;
+      if(a.match_id >=id2016+1 && a.match_id<=(end)){
+        if(extraruns[a.bowling_team]){
+            extraruns[a.bowling_team]+=a.extra_runs;
         }
         else{
-            r[a.bowling_team]=a.extra_runs;
+            extraruns[a.bowling_team]=a.extra_runs;
         }
     }
     });
-    return r;
+    return extraruns;
 }
 
-function bowlereco(deliveries,id2015,end){
-    let e={}
+
+
+function topTenEcoBowler2015(deliveries,id2015,end){
+    let TotalRunsAndBalls={}
     deliveries.forEach( a=> {
-        if(a.match_id >=id2015+1 && a.match_id<=(id2015+end)){
-          if(!e.hasOwnProperty(a.bowler)){
-                  e[a.bowler]={}
-                  e[a.bowler]["runs"]=0;
-                  e[a.bowler]["balls"]=0;
+        if(a.match_id >=id2015+1 && a.match_id<=end){
+          if(!TotalRunsAndBalls.hasOwnProperty(a["bowler"])){
+                TotalRunsAndBalls[a["bowler"]]={}
+                TotalRunsAndBalls[a["bowler"]]["runs"]=a.total_runs-a.bye_runs-a.legbye_runs
+                if(a.noball_runs==0 && a.wide_runs==0){
+                    TotalRunsAndBalls[a["bowler"]]["balls"]=1;
+                }
+                else {
+                    TotalRunsAndBalls[a["bowler"]]["balls"]=0;
+                }
+                 
           }
           else{
-              e[a.bowler]["runs"]+=a.total_runs;
-              e[a.bowler]["balls"]+=1;
+              TotalRunsAndBalls[a["bowler"]]["runs"]+=a.total_runs-a.bye_runs-a.legbye_runs
+              if(a.noball_runs==0 && a.wide_runs==0){
+                TotalRunsAndBalls[a["bowler"]]["balls"]+=1;
+              }
           }
         }
     });
-    let sort1=[]; 
-   for(let i in e){
-        temp=e[i]["runs"]/(e[i]["balls"]/6)
-        sort1.push([i,parseFloat(temp.toFixed(2))])
+    let totalRunsandOvers=[]; 
+    for(let i in TotalRunsAndBalls){
+        temp=TotalRunsAndBalls[i]["runs"]/(TotalRunsAndBalls[i]["balls"]/6)
+        totalRunsandOvers.push([i,parseFloat(temp.toFixed(2))])
     }
-    //console.log(sort1)
-    let sorted=sort1.sort( (a,b) =>{
+    let sortedArray=totalRunsandOvers.sort( (a,b) =>{
         return a[1]-b[1];
     });
-    //console.log(sorted)
-    let  out4={};
+    
+    let  topTenEcoBowlers={};
     for(let j=0;j<10;j++){
-        out4[sorted[j][0]]=sorted[j][1];
+        topTenEcoBowlers[sortedArray[j][0]]=sortedArray[j][1];
     }
-    //console.log(out4)
-    return out4;
+    console.log(topTenEcoBowlers)
+    return topTenEcoBowlers;
 }
+
+
+//exporting modules
 module.exports.noOfMatchesPerYear=noOfMatchesPerYear;
 module.exports.noOfMatchWonPerYear=noOfMatchWonPerYear;
-module.exports.extraruns=extraruns;
-module.exports.bowlereco=bowlereco;
+module.exports.extraRuns2016=extraRuns2016;
+module.exports.topTenEcoBowler2015=topTenEcoBowler2015;
