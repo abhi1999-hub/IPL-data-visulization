6d675eed0a59ca8877341f4e8ea4263e4136d234
Abhilash CR <abhilash.cr@mountblue.tech>
2020-07-28 13:42:38 +0530

    drawing UI for Total matches per year using highcharts

--- src/public/app.js
+++ src/public/app.js
@@ -10,6 +10,51 @@ function convertDataFormat(d){
      return data
 }
 featchData("matchesPerYear.json").then(res => res.json())
-/*.then((data)=>convertDataFormat(data))
-.then((data)=>drawPieChart(data))
-.catch((error)=>console.log(error));*/
+.then((data)=>convertDataFormat(data))
+.then((data)=>drawMatchesPerYear(data))
+.catch((error)=>console.log(error));
+function drawMatchesPerYear(d1){
+    Highcharts.chart('matchPerYear', {
+      chart: {
+          plotBackgroundColor: null,
+          plotBorderWidth: 0,
+          plotShadow: false
+      },
+      title: {
+          text: 'IPL<br>mathches<br>share',
+          align: 'center',
+          verticalAlign: 'middle',
+          y: 60
+      },
+      tooltip: {
+          pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+      },
+      accessibility: {
+          point: {
+              valueSuffix: '%'
+          }
+      },
+      plotOptions: {
+          pie: {
+              dataLabels: {
+                  enabled: true,
+                  distance: -50,
+                  style: {
+                      fontWeight: 'bold',
+                      color: 'white'
+                  }
+              },
+              startAngle: -90,
+              endAngle: 90,
+              center: ['50%', '75%'],
+              size: '110%'
+          }
+      },
+      series: [{
+          type: 'pie',
+          name: 'IPL matches count each year',
+          innerSize: '50%',
+          data: d1
+      }]
+  });
+  }
