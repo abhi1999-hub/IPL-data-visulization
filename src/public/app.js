47a35a472a5ce6b27d9db03f6b897f0beba50e25
Abhilash CR <abhilash.cr@mountblue.tech>
2020-07-30 23:39:56 +0530

    all problems as been implemented using one high chart function

--- src/public/app.js
+++ src/public/app.js
@@ -46,233 +46,112 @@ function convertData(d){
     console.log(output)
     return output;
     }
+
+//chart details in object for each data to plot on highcharts
+let matchesEachYear = {
+    tagId:'matchPerYear',
+    title:'Total IPL matches Played Each year',
+    titleYaxis:"Number OF Matches",
+    tooltip:{pointFormat:'matches Played: <b>{point.y:.1f}</b>'}
+   }
+let matchesWon= {
+        tagId:'matchesWonPerYear',
+        title:'Matches Won Each Year BY Each Team',
+        xAxis: {
+            categories: [2008,2009,2010,2011,2012,2013,2014,2015,2016,2017],
+            crosshair: true
+        },
+        titleYaxis:"Number OF Matches",
+        tooltip:{
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        }
+}
+let extraRuns = {
+        tagId:'extraRuns',
+        title:'Extra runs consided by each team in 2016',
+        titleYaxis:"Extra Runs",
+        tooltip:{pointFormat:'Extra Runs in 2016: <b>{point.y:.1f}</b>'},
+}
+let Top10Bowlers = {
+        tagId:'top10EcoBowlers',
+        title:'Top ten econamy bowlers in 2015',
+        titleYaxis:"Economy Rate",
+        tooltip:{pointFormat:'Economy rate in  2015: <b>{point.y:.1f} runs/over</b>'}
+}
 //fetching and ploting  matchesPerYear 
-featchData("matchesPerYear.json").then(res => res.json())
+featchData("matchesPerYear.json")
+.then(res => res.json())
 .then((data)=>convertDataFormat(data))
-.then((data)=>drawMatchesPerYear(data))
+.then((data)=>drawChart(matchesEachYear,[{
+    name: 'Years',
+    data: data
+}]))
 .catch((error)=>console.log(error));
 
 //fetching and ploting matches won per year per team
 featchData("matchesWonPerTeamPerYear.json")
 .then(res => res.json())
 .then((data)=>convertData(data))
-.then((d)=>drawMatchesWonPerYearPerTeam(d))
+.then((data)=>drawChart(matchesWon,data))
 .catch((err)=>console.log(err));
 
 //fetching and ploting top10 economy bowlers
 featchData("Top10EcoBowlers.json")
 .then(res => res.json())
 .then((data)=>convertDataFormat(data))
-.then((data)=>drawTop10EcoBowlers(data))
+.then((data)=>drawChart(Top10Bowlers,[{
+    name: 'Top 10 Bowlers',
+    data: data
+}]))
 .catch((err)=>console.log(err));
 
 //fetching and ploting ExtraRuns
 featchData("ExtraRunsPerTeam2016.json")
 .then(res => res.json())
 .then((data)=>convertDataFormat(data))
-.then((data)=>drawExtraRuns(data))
+.then((data)=>drawChart(extraRuns,[{
+    name: 'IPL Teams',
+    data: data
+}]))
 .catch((err)=>console.log(err));
 
 
-//function to plot matchesPerYear data
-function drawMatchesPerYear(d1){
-    Highcharts.chart('matchPerYear', {
-      chart: {
-          plotBackgroundColor: null,
-          plotBorderWidth: 0,
-          plotShadow: false
-      },
-      title: {
-          text: 'IPL<br>mathches<br>share',
-          align: 'center',
-          verticalAlign: 'middle',
-          y: 60
-      },
-      tooltip: {
-          pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-      },
-      accessibility: {
-          point: {
-              valueSuffix: '%'
-          }
-      },
-      plotOptions: {
-          pie: {
-              dataLabels: {
-                  enabled: true,
-                  distance: -50,
-                  style: {
-                      fontWeight: 'bold',
-                      color: 'white'
-                  }
-              },
-              startAngle: -90,
-              endAngle: 90,
-              center: ['50%', '75%'],
-              size: '110%'
-          }
-      },
-      series: [{
-          type: 'pie',
-          name: 'IPL matches count each year',
-          innerSize: '50%',
-          data: d1
-      }]
-  });
-  }
-
-//function to plot matches won per year per team
-function drawMatchesWonPerYearPerTeam(d1){
-    Highcharts.chart('matchesWonPerYear', {
-
+//highchart function to plot the chart
+function drawChart(obj,data){
+    Highcharts.chart(obj.tagId, {
+        chart: {
+            type: 'column'
+        },
         title: {
-            text: "Number of Matches Won Per Team Each Year"
+            text: obj.title
+        },
+        xAxis: obj.xAxis || {
+            type: 'category',
+            
+            labels: {
+                rotation: -45,
+                style: {
+                    fontSize: '13px',
+                    fontFamily: 'Verdana, sans-serif'
+                }
+            }
         },
         yAxis: {
+            min: 0,
             title: {
-                text: 'Number of Matches'
+                text: obj.titleYaxis
             }
         },
-    
-        xAxis: {
-            accessibility: {
-                rangeDescription: 'Range: 2008 to 2017'
-            }
-        },
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'middle'
-        },
-        plotOptions: {
-            series: {
-                label: {
-                    connectorAllowed: false
-                },
-                pointStart: 2008
-            }
-        },
-        series: d1,
-        responsive: {
-            rules: [{
-                condition: {
-                    maxWidth: 500
-                },
-                chartOptions: {
-                    legend: {
-                        layout: 'horizontal',
-                        align: 'center',
-                        verticalAlign: 'bottom'
-                    }
-                }
-            }]
-        }
-    
+        tooltip: obj.tooltip
+        ,
+        series: data,
     });
-}
-
-
-//function to plot top 10 Economy bowlers in 2015
-function drawTop10EcoBowlers(d1){
-    Highcharts.chart('top10EcoBowlers', {
-        chart: {
-            type: 'column'
-        },
-        title: {
-            text: 'Top ten econamy bowlers in 2015'
-        },
-        xAxis: {
-            type: 'category',
-            labels: {
-                rotation: -45,
-                style: {
-                    fontSize: '13px',
-                    fontFamily: 'Verdana, sans-serif'
-                }
-            }
-        },
-        yAxis: {
-            min: 0,
-            title: {
-                text: 'Economy rate'
-            }
-        },
-        legend: {
-            enabled: false
-        },
-        tooltip: {
-            pointFormat: 'Economy rate in  2015: <b>{point.y:.1f} runs/over</b>'
-        },
-        series: [{
-            name: 'Bowlers',
-            data: d1
-        }],
-    
-                
-            dataLabels: {
-                enabled: true,
-                rotation: -90,
-                color: '#FFFFFF',
-                align: 'right',
-                format: '{point.y:.1f}', // one decimal
-                y: 10, // 10 pixels down from the top
-                style: {
-                    fontSize: '13px',
-                    fontFamily: 'Verdana, sans-serif'
-                }
-            }
-        });
-}
-
-
-//function to plot Extra Runs concided per team in year 2016
-function drawExtraRuns(d1){
-    Highcharts.chart('extraRuns', {
-        chart: {
-            type: 'column'
-        },
-        title: {
-            text: 'Extra runs consided by each team in 2016'
-        },
-        xAxis: {
-            type: 'category',
-            labels: {
-                rotation: -45,
-                style: {
-                    fontSize: '13px',
-                    fontFamily: 'Verdana, sans-serif'
-                }
-            }
-        },
-        yAxis: {
-            min: 0,
-            title: {
-                text: 'Extra Runs'
-            }
-        },
-        legend: {
-            enabled: false
-        },
-        tooltip: {
-            pointFormat: 'Extra Runs in 2016: <b>{point.y:.1f}</b>'
-        },
-        series: [{
-            name: 'Teams',
-            data: d1
-        }],
-    
-                
-            dataLabels: {
-                enabled: true,
-                rotation: -90,
-                color: '#FFFFFF',
-                align: 'right',
-                format: '{point.y:.1f}', // one decimal
-                y: 10, // 10 pixels down from the top
-                style: {
-                    fontSize: '13px',
-                    fontFamily: 'Verdana, sans-serif'
-                }
-            }
-        });
 } 
+
+
+
