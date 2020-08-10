7657ce2dd3e7ae71efec7bbfe2f02d246133784f
Abhilash CR <abhilash.cr@mountblue.tech>
2020-08-10 13:11:44 +0530

    final changes

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,8 @@
 const mysql=require('mysql');
 const { result }=require("./config.js")
 const func= require('./ipl')
+
+//creating connection with database
 const database=mysql.createConnection({
     host :result.parsed.host,
     user:result.parsed.user,
@@ -21,8 +23,12 @@ const connectDatabase = ()=>{
 connectDatabase().then((output)=>{
     console.log(output)
 }).catch(console.log)
+
+//required function
 func.noOfMatchesPerYear(database)
 func.noOfMatchWonPerYear(database)
 func.extraRuns2016(database)
 func.topTenEcoBowler2015(database)
+
+//disconnecting from database
 database.end()
--- src/server/ipl.js
+++ src/server/ipl.js
@@ -1,5 +1,6 @@
 const fs=require('fs');
-function connectdatabase(database,sql){
+//This function will retrive data from database for given query
+function reteriveData(database,sql){
     return new Promise((resolve,reject)=>(
        database.query(sql,(err,results,fields)=>{
            if(err){
@@ -8,30 +9,38 @@ function connectdatabase(database,sql){
            resolve(results)
    })
 ))}
-   
+
+//Query to retrive number of matches per year
 function noOfMatchesPerYear(database){
     console.log("hai")
     const query="SELECT season, count(season) AS numberOFMatches FROM matches GROUP BY season ORDER BY season"
-    connectdatabase(database,query).then((output)=>fs.writeFile('..//output//matchesPerYear.json',JSON.stringify(output), (err)=>
-    console.log("matchesPerYear File written")))
+    reteriveData(database,query).then((output)=>fs.writeFile('..//output//matchesPerYear.json',JSON.stringify(output), (err)=>
+    console.log("matchesPerYear File written"))).catch(console.log)
 }
+//Query to retrive number of matches won each year by each team
 function noOfMatchWonPerYear(database){
     const query="SELECT season,winner as Team, count(*) as matches_won FROM matches GROUP BY winner,season"
-    return connectdatabase(database,query).then((output)=>fs.writeFile('..//output//matchesWonPerTeamPerYear.json',JSON.stringify(output), (err)=>
-    console.log("matchesWonPerTeamPerYear File written")))
+    reteriveData(database,query).then((output)=>fs.writeFile('..//output//matchesWonPerTeamPerYear.json',JSON.stringify(output), (err)=>
+    console.log("matchesWonPerTeamPerYear File written"))).catch(console.log)
 }
+
+//Query to retrive extra runs by each team in 2016
 function extraRuns2016(database){
     const query="SELECT bowling_team AS team , sum(extra_runs) AS Extra_runs FROM deliveries WHERE match_id IN (SELECT id FROM matches WHERE season=2016) GROUP BY(bowling_team)"
-    return connectdatabase(database,query).then((output)=>fs.writeFile('..//output//ExtraRunsPerTeam2016.json',JSON.stringify(output), (err)=>
-    console.log("ExtraRunsPerTeam2016 File written")))
+    reteriveData(database,query).then((output)=>fs.writeFile('..//output//ExtraRunsPerTeam2016.json',JSON.stringify(output), (err)=>
+    console.log("ExtraRunsPerTeam2016 File written"))).catch(console.log)
 }
+
+//Query to retrive Top 10 Econamy Bowlers  in 2015
 function topTenEcoBowler2015(database){
     const query="SELECT deliveries.bowler, sum(deliveries.total_runs-deliveries.bye_runs-deliveries.legbye_runs)/(count(a1.id)/6) AS econamy FROM deliveries LEFT JOIN (SELECT * FROM deliveries WHERE noball_runs=0 and wide_runs=0) AS a1 ON deliveries.id=a1.id WHERE deliveries.match_id in (SELECT id FROM matches WHERE season=2015) GROUP BY(deliveries.bowler) ORDER BY econamy LIMIT 10"
      
-    connectdatabase(database,query).then((output)=>fs.writeFile('..//output//TopTenEcoBowlers.json',JSON.stringify(output), (err)=>
-    console.log("TopTenECOBowlers2015 File written")))
+    reteriveData(database,query).then((output)=>fs.writeFile('..//output//TopTenEcoBowlers.json',JSON.stringify(output), (err)=>
+    console.log("TopTenECOBowlers2015 File written"))).catch(console.log)
 
 }
+
+//exporting modules 
 module.exports.noOfMatchesPerYear=noOfMatchesPerYear;
 module.exports.noOfMatchWonPerYear=noOfMatchWonPerYear;
 module.exports.extraRuns2016=extraRuns2016;
